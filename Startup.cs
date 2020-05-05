a3fcaf35f3626a44e1398c4479537f43969e3865
Javier Juarez Moctezuma <juarezja@hotmail.com>
2020-05-04 22:10:54 -0500

    Hasta lección 37

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using vega.Persistence;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace vega
 {
@@ -22,6 +23,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
